6af32e359de92e49508e3ff37e3084b38e3debc5
frank <frankluck52@gmail.com>
2017-04-25 21:43:13 -0700

    socket server/client test

--- p0/src/github.com/cmu440/crunner/crunner.go
+++ p0/src/github.com/cmu440/crunner/crunner.go
@@ -1,12 +1,16 @@
 package main
 
 import (
-    "fmt"
+	"bufio"
+	"fmt"
+	"net"
+	"strconv"
+	"sync"
 )
 
 const (
-    defaultHost = "localhost"
-    defaultPort = 9999
+	defaultHost = "localhost"
+	defaultPort = 9999
 )
 
 // To test your server implementation, you might find it helpful to implement a
@@ -15,5 +19,35 @@ const (
 // read and print out the server's response to standard output. Whether or
 // not you add any code to this file will not affect your grade.
 func main() {
-    fmt.Println("Not implemented.")
+	// connect to this socket
+	conn, err := net.Dial("tcp", defaultHost+":"+strconv.Itoa(defaultPort))
+	if err != nil {
+		fmt.Println("Error connecting:", err)
+	}
+	defer conn.Close()
+	fmt.Println("Connecting to " + defaultHost + ":" + strconv.Itoa(defaultPort))
+	var wg sync.WaitGroup
+	wg.Add(2)
+	go handleWrite(conn, &wg)
+	go handleRead(conn, &wg)
+	wg.Wait()
+}
+func handleWrite(conn net.Conn, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for i := 1; i > 0; i-- {
+		_, e := conn.Write([]byte("hello " + strconv.Itoa(i) + "\r\n"))
+		if e != nil {
+			fmt.Println("Error to send message because of ", e.Error())
+			break
+		}
+	}
+}
+func handleRead(conn net.Conn, wg *sync.WaitGroup) {
+	defer wg.Done()
+	reader := bufio.NewReader(conn)
+	for i := 1; i <= 10; i++ {
+		line, _ := reader.ReadString(byte('\n'))
+
+		fmt.Print(line)
+	}
 }
--- p0/src/github.com/cmu440/p0/server_impl.go
+++ p0/src/github.com/cmu440/p0/server_impl.go
@@ -2,28 +2,58 @@
 
 package p0
 
+import (
+	"fmt"
+	"net"
+	"strconv"
+)
+
 type keyValueServer struct {
-    // TODO: implement this!
+	value int
 }
 
 // New creates and returns (but does not start) a new KeyValueServer.
 func New() KeyValueServer {
-    // TODO: implement this!
-    return nil
+	return &keyValueServer{1}
 }
 
 func (kvs *keyValueServer) Start(port int) error {
-    // TODO: implement this!
-    return nil
+	ln, err := net.Listen("tcp", ":"+strconv.Itoa(port))
+	if err != nil {
+		return err
+	}
+	defer ln.Close()
+	fmt.Println("Listening on " + strconv.Itoa(port))
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			fmt.Println("Error accepting: ", err.Error())
+			return nil
+		}
+		go handleRequest(conn)
+	}
+	return nil
 }
 
 func (kvs *keyValueServer) Close() {
-    // TODO: implement this!
+	// TODO: implement this!
 }
 
 func (kvs *keyValueServer) Count() int {
-    // TODO: implement this!
-    return -1
+	// TODO: implement this!
+	return -1
 }
 
 // TODO: add additional methods/functions below!
+func handleRequest(conn net.Conn) {
+	buf := make([]byte, 1024)
+	// Read the incoming connection into the buffer.
+	_, err := conn.Read(buf)
+	if err != nil {
+		fmt.Println("Error reading:", err.Error())
+	}
+	// Send a response back to person contacting us.
+	conn.Write([]byte("Message received."))
+	// Close the connection when you're done with it.
+	conn.Close()
+}
